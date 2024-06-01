6d626b7f7affcae96337183981483a8129c3b436
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-01 13:11:16 -0700

    fixed exercise-one.js

--- lesson04/exercise-one.js
+++ lesson04/exercise-one.js
@@ -1,4 +1,4 @@
-let hotDog = ['relish', 'mustard', 'ketchup', 'onions', 'sauerkraut']
+let hotDog = ['relish', 'mustard', 'ketchup', 'onions', 'sauerkraut'];
 hotDog.push('mayo');
 hotDog.splice(2, 1);
 console.log(hotDog.join());
