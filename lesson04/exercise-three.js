91a7329d76c27a1f8244a59dfcc9f8cec9586777
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-01 21:30:16 -0700

    created excercise-three.js and added code.

---
+++ lesson04/exercise-three.js
@@ -0,0 +1,5 @@
+let hotDog = ['relish', 'mustard', 'ketchup', 'onions', 'sauerkraut'];
+for (let i = 0; i < hotDog.length; i++) {
+  let hotDogList = i + ',' + (hotDog[i]);
+  console.log(hotDogList);
+}
