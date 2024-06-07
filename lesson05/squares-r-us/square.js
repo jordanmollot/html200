d364693fac04bcdac4df7be307102140cab27d74
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-07 14:16:43 -0700

    created squares-r-us directory and index.html and squares.js in that directory

---
+++ lesson05/squares-r-us/square.js
@@ -0,0 +1,9 @@
+function numberAdder() {
+    let input = prompt('Enter a number');
+    let numberToAdd = 2;
+    let result = 0;
+    
+    result = numberToAdd + Number(input);
+    
+    alert(result);
+}
