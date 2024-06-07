32251a0c30d1168982ac95343e13ef5e00fd6609
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-07 14:39:38 -0700

    added ability to multiply 2 numbers input by a user

--- lesson05/squares-r-us/square.js
+++ lesson05/squares-r-us/square.js
@@ -7,3 +7,13 @@ function numberSquared() {
     
     alert(result);
 }
+
+function multiplyNumbers() {
+    let input = prompt('Enter 2 numbers (separated by a space) that you want to multiply');
+    let inputNumbers = input.split(' ')
+    let result = 0;
+    
+    result = inputNumbers[0] * inputNumbers[1];
+    
+    alert(result);
+}
